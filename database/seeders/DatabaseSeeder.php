5b4d1243938631410c1b5e6be47bd6bb3980bad4
Angelo Sosa Macuyama <995517771angelo@gmail.com>
2026-07-02 05:30:26 -0500

    Configurar trusted proxies y corregir DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,11 +22,11 @@ class DatabaseSeeder extends Seeder
 
         // User::factory(10)->create();
 
-        User::firstOrCreate(
-            ['email' => 'test@example.com'],
-            [
+        if (! User::where('email', 'test@example.com')->exists()) {
+            User::factory()->create([
                 'name' => 'Test User',
-            ]
-        );
+                'email' => 'test@example.com',
+            ]);
+        }
     }
 }
